12e4a5ffcd8e29a24a8267d175267eac384a44c6
BrunaRihl <brunarihl@gmail.com>
2024-05-22 11:21:39 +0100

    Add interactive star rating feature and style

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,3 @@
-/* 
-   Handles the interactive star rating feature. 
-   It listens for mouse events on star icons to highlight them 
-   based on the user's interaction.
-*/
-document.addEventListener('DOMContentLoaded', () => {
-    const stars = document.querySelectorAll('.rating .fa-star');
-    const ratingInput = document.getElementById('rating');
-
-    stars.forEach(star => {
-      star.addEventListener('mouseover', (e) => {
-        const value = e.target.dataset.value;
-        highlightStars(value);
-      });
-
-      star.addEventListener('mouseout', () => {
-        highlightStars(ratingInput.value || 0);
-      });
-
-      star.addEventListener('click', (e) => {
-        const value = e.target.dataset.value;
-        ratingInput.value = value;
-        highlightStars(value);
-      });
-    });
-
-    function highlightStars(value) {
-      stars.forEach((star, index) => {
-        if (index < value) {
-          star.classList.add('selected');
-          star.classList.remove('fa-regular');
-          star.classList.add('fa-solid');
-        } else {
-          star.classList.remove('selected');
-          star.classList.remove('fa-solid');
-          star.classList.add('fa-regular');
-        }
-      });
-    }
-  });
-  
 
 /* 
    Creates a dynamic glitter effect 
@@ -60,4 +19,43 @@ document.addEventListener("DOMContentLoaded", function() {
   setInterval(createGlitterParticle, 200);
 });
 
-  
+/* 
+   Handles the interactive star rating feature. 
+   It listens for mouse events on star icons to highlight them 
+   based on the user's interaction.
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  const stars = document.querySelectorAll('.rating .fa-star');
+  const ratingInput = document.getElementById('rating');
+
+  stars.forEach(star => {
+    star.addEventListener('mouseover', (e) => {
+      const value = e.target.dataset.value;
+      highlightStars(value);
+    });
+
+    star.addEventListener('mouseout', () => {
+      highlightStars(ratingInput.value || 0);
+    });
+
+    star.addEventListener('click', (e) => {
+      const value = e.target.dataset.value;
+      ratingInput.value = value;
+      highlightStars(value);
+    });
+  });
+
+  function highlightStars(value) {
+    stars.forEach((star, index) => {
+      if (index < value) {
+        star.classList.add('selected');
+        star.classList.remove('fa-regular');
+        star.classList.add('fa-solid');
+      } else {
+        star.classList.remove('selected');
+        star.classList.remove('fa-solid');
+        star.classList.add('fa-regular');
+      }
+    });
+  }
+});
